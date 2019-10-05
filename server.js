5dcc4bd5ee177aef27fcaf37bcf5fdaefd195f45
Spencer Pauly <spencerpauly@gmail.com>
2019-10-05 16:05:48 -0500

    Fixed issue w/ compromised security token. Reset client secret.

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ var app = express();
 
 // init Spotify API wrapper
 var PORT = 80;
-var CLIENT_ID = '7182842a6a664b03917c1ca416af7453';
-var CLIENT_SECRET = 'd87d7427af46422486152526fd5903c0';
-var URL = 'http://localhost:' + PORT + '';
+var CLIENT_ID = '';
+var CLIENT_SECRET = '';
+var URL = 'https://server.spencerpauly.com';
 
 var SpotifyWebApi = require('spotify-web-api-node');
 
