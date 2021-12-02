3503f2fcd158445c18f10b3461eb9de94f3a1986
spencerpauly <spencerpauly@gmail.com>
2021-12-02 07:28:29 -0600

    Better env variables

--- public/js/index.js
+++ public/js/index.js
@@ -2,37 +2,43 @@ const LOCALSTORAGE_ACCESS_TOKEN_KEY = 'spotify-audio-analysis-playback-token';
 const LOCALSTORAGE_ACCESS_TOKEN_EXPIRY_KEY = 'spotify-audio-analysis-playback-token-expires-in';
 
 function parseHash(hash) {
-  return hash
-    .substring(1)
-    .split('&')
-    .reduce(function (initial, item) {
-      if (item) {
-        var parts = item.split('=');
-        initial[parts[0]] = decodeURIComponent(parts[1]);
-      }
-      return initial;
-    }, {});
+	return hash
+		.substring(1)
+		.split('&')
+		.reduce(function (initial, item) {
+			if (item) {
+				var parts = item.split('=');
+				initial[parts[0]] = decodeURIComponent(parts[1]);
+			}
+			return initial;
+		}, {});
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN_KEY) &&
-      parseInt(parseInt(localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN_EXPIRY_KEY))) > Date.now()) {
-    window.location = "/spotify/analysis.html";
-  } else {
-    if(window.location.hash) {
-      const hash = parseHash(window.location.hash);
-      if (hash['access_token'] &&
-          hash['expires_in']) {
-        localStorage.setItem(LOCALSTORAGE_ACCESS_TOKEN_KEY, hash['access_token']);
-        localStorage.setItem(LOCALSTORAGE_ACCESS_TOKEN_EXPIRY_KEY, Date.now() + 990 * parseInt(hash['expires_in']));
-        window.location = "/spotify/analysis.html";
-      }
-    } 
-    fetch('/spotifyRedirectUri')
-      .then(e => e.json())
-      .then(data => {
-            window.location = data.redirectUri;
-      })
-      .catch(error => { alert("Failed to prepare for Spotify Authentication")});
-  }
-})
+	if (
+		localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN_KEY) &&
+		parseInt(parseInt(localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN_EXPIRY_KEY))) > Date.now()
+	) {
+		window.location = '/spotify/analysis.html';
+	} else {
+		if (window.location.hash) {
+			const hash = parseHash(window.location.hash);
+			if (hash['access_token'] && hash['expires_in']) {
+				localStorage.setItem(LOCALSTORAGE_ACCESS_TOKEN_KEY, hash['access_token']);
+				localStorage.setItem(
+					LOCALSTORAGE_ACCESS_TOKEN_EXPIRY_KEY,
+					Date.now() + 990 * parseInt(hash['expires_in'])
+				);
+				window.location = '/spotify/analysis.html';
+			}
+		}
+		fetch('/spotifyRedirectUri')
+			.then((r) => r.json())
+			.then((data) => {
+				window.location = data.redirectUri;
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+});
--- server.js
+++ server.js
@@ -1,109 +1,137 @@
 // server.js
 // where your node app starts
-
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 // init project
 var qs = require('querystring');
 var express = require('express');
 var app = express();
 
 // init Spotify API wrapper
-<<<<<<< HEAD
 var PORT = 3001;
-var CLIENT_ID = '';
-var CLIENT_SECRET = '';
+var CLIENT_ID = process.env.CLIENT_ID;
+var CLIENT_SECRET = process.env.CLIENT_SECRET;
 var REDIRECT_URL = 'https://spotify.spencerpauly.com';
-=======
-var PORT = 80;
-var CLIENT_ID = '';
-var CLIENT_SECRET = '';
-var URL = 'https://server.spencerpauly.com';
->>>>>>> 5dcc4bd5ee177aef27fcaf37bcf5fdaefd195f45
 
 var SpotifyWebApi = require('spotify-web-api-node');
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : CLIENT_ID,
-  clientSecret : CLIENT_SECRET,
+	clientId: CLIENT_ID,
+	clientSecret: CLIENT_SECRET,
 });
 
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
 
-const oldScopes = ["ugc-image-upload", "streaming", "user-library-modify", "user-library-read", "user-read-birthdate", "user-read-email", "user-read-private", "user-modify-playback-state", "user-read-private", 
-"user-read-private", "user-top-read", "user-read-recently-played", "playlist-modify-public", "playlist-modify-private", "playlist-read-collaborative", "playlist-read-private"];
+const oldScopes = [
+	'ugc-image-upload',
+	'streaming',
+	'user-library-modify',
+	'user-library-read',
+	'user-read-birthdate',
+	'user-read-email',
+	'user-read-private',
+	'user-modify-playback-state',
+	'user-read-private',
+	'user-read-private',
+	'user-top-read',
+	'user-read-recently-played',
+	'playlist-modify-public',
+	'playlist-modify-private',
+	'playlist-read-collaborative',
+	'playlist-read-private',
+];
 
-const jssdkscopes = ["user-library-modify", "user-library-read", "user-read-email", "user-read-private", "user-modify-playback-state", "user-read-private", 
-                     "user-read-private", "user-top-read", "playlist-modify-public", "playlist-modify-private", "playlist-read-collaborative", "playlist-read-private"];
+const jssdkscopes = [
+	'user-library-modify',
+	'user-library-read',
+	'user-read-email',
+	'user-read-private',
+	'user-modify-playback-state',
+	'user-read-private',
+	'user-read-private',
+	'user-top-read',
+	'playlist-modify-public',
+	'playlist-modify-private',
+	'playlist-read-collaborative',
+	'playlist-read-private',
+];
 const redirectUriParameters = {
-  client_id: CLIENT_ID,
-  response_type: 'token',
-  scope: jssdkscopes.join(' '),
-  redirect_uri: encodeURI(REDIRECT_URL),
-  show_dialog: true,
-}
+	client_id: CLIENT_ID,
+	response_type: 'token',
+	scope: jssdkscopes.join(' '),
+	redirect_uri: encodeURI(REDIRECT_URL),
+	show_dialog: true,
+};
 
 const redirectUri = `https://accounts.spotify.com/authorize?${qs.stringify(redirectUriParameters)}`;
 
 function authenticate(callback) {
-  spotifyApi.clientCredentialsGrant()
-    .then(function(data) {
-      console.log('The access token expires in ' + data.body['expires_in']);
-      console.log('The access token is ' + data.body['access_token']);
-    
-      callback instanceof Function && callback();
+	spotifyApi.clientCredentialsGrant().then(
+		function (data) {
+			console.log('The access token expires in ' + data.body['expires_in']);
+			console.log('The access token is ' + data.body['access_token']);
 
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-    }, function(err) {
-      console.log('Something went wrong when retrieving an access token', err.message);
-    });
+			callback instanceof Function && callback();
+
+			// Save the access token so that it's used in future calls
+			spotifyApi.setAccessToken(data.body['access_token']);
+		},
+		function (err) {
+			console.log('Something went wrong when retrieving an access token', err.message);
+		}
+	);
 }
 authenticate();
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.get("/search", function (request, response) {
-  reAuthenticateOnFailure((failure) => {
-    spotifyApi.searchTracks(request.query.query, { limit: 2 })
-    .then(function(data) {
-      response.send(data.body);
-    }, failure);
-  })
+app.get('/search', function (request, response) {
+	reAuthenticateOnFailure((failure) => {
+		spotifyApi.searchTracks(request.query.query, { limit: 2 }).then(function (data) {
+			response.send(data.body);
+		}, failure);
+	});
 });
 
 const reAuthenticateOnFailure = (action) => {
-  action(() => {
-    authenticate(action);
-  })
-}
+	action(() => {
+		authenticate(action);
+	});
+};
 
-app.get("/spotifyRedirectUri", function (request, response) {
-  response.send(JSON.stringify({
-    redirectUri
-  }, null, 2))
+app.get('/spotifyRedirectUri', function (request, response) {
+	response.send(
+		JSON.stringify(
+			{
+				redirectUri,
+			},
+			null,
+			2
+		)
+	);
 });
 
-app.get("/spotify/features", function (request, response) {
-  reAuthenticateOnFailure((failure) => {
-    spotifyApi.getAudioFeaturesForTrack(request.query.id)
-    .then(function(data) {
-      response.send(data.body);
-    }, failure);
-  })
+app.get('/spotify/features', function (request, response) {
+	reAuthenticateOnFailure((failure) => {
+		spotifyApi.getAudioFeaturesForTrack(request.query.id).then(function (data) {
+			response.send(data.body);
+		}, failure);
+	});
 });
 
-app.get("/spotify/analysis", function (request, response) {
-  reAuthenticateOnFailure((failure) => {
-    spotifyApi.getAudioAnalysisForTrack(request.query.id)
-    .then(function(data) {
-      response.send(data.body);
-    }, failure);
-  });
+app.get('/spotify/analysis', function (request, response) {
+	reAuthenticateOnFailure((failure) => {
+		spotifyApi.getAudioAnalysisForTrack(request.query.id).then(function (data) {
+			response.send(data.body);
+		}, failure);
+	});
 });
 
 // listen for requests :)
 var listener = app.listen(PORT, function () {
-  console.log('Your app is listening on port ' + PORT);
-  console.log(appDir);
+	console.log('Your app is listening on port ' + PORT);
+	console.log(appDir);
 });
